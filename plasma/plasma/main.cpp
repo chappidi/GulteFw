06a328464b87fd38c72b0bcba30a4a972d4f7d90
chappidi <chappidi@live.com>
2020-01-18 18:51:00 -0500

    initial commit

---
+++ plasma/plasma/main.cpp
@@ -0,0 +1,12 @@
+﻿// plasma.cpp : Defines the entry point for the application.
+//
+
+#include "plasma.h"
+#include <plasma_client/NewOrderSingle.h>
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
