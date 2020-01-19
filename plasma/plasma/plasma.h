a5dea91fda773923bcf34a51635d9cd49838cc22
chappidi <chappidi@live.com>
2020-01-18 22:31:49 -0500

    gtest added

--- plasma/plasma/plasma.h
+++ plasma/plasma/plasma.h
@@ -1,8 +1,40 @@
-﻿// plasma.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
+﻿#pragma once
+#include <plasma_client/NewOrderSingle.h>
+#include <plasma_client/OrderCancelRequest.h>
+#include <plasma_client/OrderReplaceRequest.h>
+#include <plasma_client/OrderStatusRequest.h>
+#include <plasma_client/OrderCancelReject.h>
+#include <plasma_client/NonFillReport.h>
+#include <plasma_client/FillReport.h>
+#include <plasma_client/DontKnowTrade.h>
 #include <iostream>
 
-// TODO: Reference additional headers your program requires here.
+using namespace plasma::client;
+namespace plasma
+{
+	class OMS
+	{
+	public:
+		void OnMsg(const NewOrderSingle& req) {
+		}
+		void OnMsg(const OrderStatusRequest& req) {
+		}
+		void OnMsg(const OrderCancelRequest& req) {
+		}
+		void OnMsg(const OrderReplaceRequest& req) {
+		}
+		void OnMsg(const NonFillReport& rpt) {
+		}
+		void OnMsg(const FillReport& rpt) {
+		}
+		void OnMsg(const OrderCancelReject& rpt) {
+		}
+		void OnMsg(const DontKnowTrade& rpt) {
+
+		}
+		void OnLogin() {
+		}
+		void OnLogout() {
+		}
+	};
+}
