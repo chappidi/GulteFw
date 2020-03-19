673046862b0664a1985c11db3c4d81be15655cc1
chappidi <chappidi@live.com>
2020-03-19 09:10:26 -0400

    no message

--- qep/RxMain/main.cpp
+++ qep/RxMain/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <rxcpp/rx.hpp>
+
 namespace Rx {
 	using namespace rxcpp;
 	using namespace rxcpp::sources;
@@ -7,9 +8,27 @@ namespace Rx {
 	using namespace rxcpp::util;
 }
 using namespace Rx;
+
 using namespace std;
+
+void test_2() {
+	auto ints = rxcpp::observable<>::create<int>(
+		[](rxcpp::subscriber<int> s) {
+			for (int itr = 1; itr < 100; ++itr) {
+				s.on_next(itr);
+				std::this_thread::sleep_for(std::chrono::milliseconds(70));
+			}
+			s.on_completed();
+		});
+	auto mnz = ints | last();
+	auto xyz = mnz | subscribe<int>(
+		[](int  val) {
+			std::cout << "create" << val << std::endl;
+		});
+}
 int main()
 {
+	test_2();
 	cout << "Hello CMake." << endl;
 
 }
